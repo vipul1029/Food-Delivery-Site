a34f379ef168f9762039c305640459ca1b9258d0
Vipul Kumar <tovipul.kr@gmail.com>
2025-05-25 22:30:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import Navbar from './components/Navbar/Navbar'
+import {Route, Routes} from 'react-router-dom'
+import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
+import Home from './Pages/Home/Home'
+import Cart from './Pages/Cart/Cart'
+import Footer from './components/Footer/Footer'
+import LoginPopup from './components/LoginPopup/LoginPopup'
+
+
+
+const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+
+  return (
+    <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+      <div className='app'>
+      <Navbar setShowLogin={setShowLogin} />
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order' element={<PlaceOrder/>}/>
+      </Routes>
+    
+    </div>
+    <Footer/>
+    </>
+  
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {BrowserRouter} from 'react-router-dom'
+import StoreContextProvider from './context/StoreContext.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+  <BrowserRouter>
+  <StoreContextProvider>
+  <App />
+  </StoreContextProvider>
+    </BrowserRouter>
+
+)
